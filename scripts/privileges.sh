68933072b45b3d372599fb15c41000bef007b955
sujatadhl <dahalsujata52@gmail.com>
2024-07-17 13:45:45 +0545

    updated script

--- scripts/privileges.sh
+++ scripts/privileges.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
 echo "before install"
-# chmod +x /home/ubuntu/app/* jar
-# chmod +x /home/ubuntu/app/server_start.sh
-# chmod +x /home/ubuntu/app/server_stop.sh
-# chmod +x /home/ubuntu/app/server_clear.sh
